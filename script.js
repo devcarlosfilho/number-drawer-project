acd5fb02499289667391d777dee592fa603d4f28
devcarlosfilho <devcarlosfilho@outlook.com>
2024-04-03 16:20:37 -0300

    adicionado funcao de valor minimo e maximo no script.js

--- script.js
+++ script.js
@@ -6,8 +6,15 @@ function generateNumber(){
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min
-    document.querySelector(".input-result").value = result
+    if(min < max){
+        const result = Math.floor(Math.random() * (max - min + 1)) + min
+        document.querySelector(".input-result").value = result
+        
+    } else{
+        alert("O valor minimo deve ser menor que o valor máximo")
+    }
+
+    
     
 }
 
